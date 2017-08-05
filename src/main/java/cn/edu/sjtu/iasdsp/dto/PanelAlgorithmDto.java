89269b7fe7f25fe8505f0f144561195ab3bdf966
lanecatm <lanecatm@sjtu.edu.cn>
2017-08-05 20:28:50 +0800

    add default value and index in panel

--- src/main/java/cn/edu/sjtu/iasdsp/dto/PanelAlgorithmDto.java
+++ src/main/java/cn/edu/sjtu/iasdsp/dto/PanelAlgorithmDto.java
@@ -1,12 +1,9 @@
 package cn.edu.sjtu.iasdsp.dto;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-
-import cn.edu.sjtu.iasdsp.model.NodeOption;
-import cn.edu.sjtu.iasdsp.model.NodeOptionChoice;
-import cn.edu.sjtu.iasdsp.model.NodeOptionType;
-import cn.edu.sjtu.iasdsp.model.NodeOptionValue;
 
 /** 
 * @author xfhuang 
@@ -36,10 +33,10 @@ public class PanelAlgorithmDto {
 		private String description;
 		private String defaultValue;
 		private String nodeOptionTypeName;
-		private Set<PanelChoice> panelChoices = new HashSet<PanelChoice>(0);
+		private List<PanelChoice> panelChoices = new ArrayList<PanelChoice>(0);
 		
 		public PanelOption(Integer nodeId, Integer nodeIndex, String name, String label, String description, String defaultValue,
-				String nodeOptionTypeName, Set<PanelChoice> panelChoices) {
+				String nodeOptionTypeName, List<PanelChoice> panelChoices) {
 			this.nodeId = nodeId;
 			this.nodeIndex = nodeIndex;
 			this.name = name;
@@ -97,18 +94,28 @@ public class PanelAlgorithmDto {
 		public void setNodeOptionTypeName(String nodeOptionTypeName) {
 			this.nodeOptionTypeName = nodeOptionTypeName;
 		}
-		public Set<PanelChoice> getPanelChoices() {
+		public List<PanelChoice> getPanelChoices() {
 			return panelChoices;
 		}
-		public void setPanelChoices(Set<PanelChoice> panelChoices) {
+		public void setPanelChoices(List<PanelChoice> panelChoices) {
 			this.panelChoices = panelChoices;
 		}
 		
 	}
 	
 	public class PanelChoice{
+		Integer id;
 		String name;
 		String value;
+		
+		
+		
+		public Integer getId() {
+			return id;
+		}
+		public void setId(Integer id) {
+			this.id = id;
+		}
 		public String getName() {
 			return name;
 		}
@@ -121,7 +128,8 @@ public class PanelAlgorithmDto {
 		public void setValue(String value) {
 			this.value = value;
 		}
-		public PanelChoice(String name, String value) {
+		public PanelChoice(Integer id, String name, String value) {
+			this.id = id;
 			this.name = name;
 			this.value = value;
 		}
